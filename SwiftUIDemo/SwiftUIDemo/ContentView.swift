82a67d60cf9c95cf949ab22b44e0e9903ba49fca
Yuliia Engman <yuliiaengmanpursuit@Yuliias-MacBook-Pro.local>
2019-09-24 15:51:56 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Yuliia Engman on 9/24/19.
+//  Copyright © 2019 Yuliia Engman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
